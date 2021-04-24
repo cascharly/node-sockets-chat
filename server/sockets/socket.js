75ffe60f599d2290ef646d49ccfcace2d974d171
cascharly <cascharly@gmail.com>
2021-04-24 10:59:42 -0300

    Front End Conectar Usuario

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,42 +1,19 @@
-const { io } = require('../server');
+const { io } = require("../server");
+const { Usuarios } = require("../classes/usuarios");
 
+const usuarios = new Usuarios();
 
-io.on('connection', (client) => {
+io.on("connection", (client) => {
+  client.on("entrarChat", (data, callback) => {
+    if (!data.nombre) {
+      return callback({
+        error: true,
+        mensaje: "El nombre es necesario",
+      });
+    }
 
-    console.log('Usuario conectado');
+    let personas = usuarios.agregarPersona(client.id, data.nombre);
 
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
-    });
-
-
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
-
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
-
-        console.log(data);
-
-        client.broadcast.emit('enviarMensaje', data);
-
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
-
-    });
-
+    callback(personas);
+  });
 });
